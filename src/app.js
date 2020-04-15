e7cf601c2eab8fb300c7a11f55d7fba4fa64e18d
Eduardo Ernesto Corrales Reyna <eduardo.reyna@syonet.com>
2020-04-14 22:40:44 -0300

    Desafio completado

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { uuid, isUuid } = require('uuidv4')
+const { uuid } = require('uuidv4')
 
 // const { uuid } = require("uuidv4");
 
